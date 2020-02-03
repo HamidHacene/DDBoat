56e3a081f855f29d604f914cb32f6cde6ff51cc8
ColinBaumgard <colin.baumgard@gmail.com>
2020-02-03 18:48:43 +0100

    adding files

---
+++ src/drivers/arduino_drivers/src/arduino.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <unistd.h>
+
+#include "serial/serial.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
+
+using namespace std;
+
+string port = "/dev/ttyACM0";
+int baudrate = 115200;
+
+void sleep(unsigned long miliseconds);
+int cast_cmd(int cmd);
+
+// Attention à bien inclure chaque type de message !
+int main(int argc, char **argv)
+{
+    // Initialisation du node : le troisième argument est son nom
+    serial::Serial arduino(port, baudrate, serial::Timeout::simpleTimeout(1000));
+    cout << "Is the serial port open?";
+    if (my_serial.isOpen())
+    {
+        cout << " Yes." << endl;
+    }
+    else
+    {
+        cout << " No." << endl;
+    }
+        
+
+    ros::NodeHandle n;
+
+    return 0;
+}
+
+void sleep(unsigned long miliseconds)
+{
+    usleep(miliseconds * 1000);
+}
+
+
+int cast_cmd(int cmd)
+{
+    if(cmd > 255)
+    {
+        return 255; 
+    }
+    else if(cmd < 0)
+    {
+        return 0;
+    }
+}
