518a4899036d4bba4a592f4c01fe3b61c7e86e53
ColinBaumgard <colin.baumgard@gmail.com>
2020-02-04 12:13:31 +0100

    motor upgrade 2

--- src/drivers/arduino_drivers/src/arduino.cpp
+++ src/drivers/arduino_drivers/src/arduino.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 string port = "/dev/ttyACM0";
 int baudrate = 115200;
+ serial::Serial arduino(port, baudrate, serial::Timeout::simpleTimeout(1000));
 
 void sleep(unsigned long miliseconds);
 int cast_cmd(int cmd);
@@ -23,7 +24,7 @@ void send_arduino_motor_cmd(serial::Serial arduino, int cmdl, int cmdr);
 int main(int argc, char **argv)
 {
     // Initialisation du node : le troisième argument est son nom
-    serial::Serial arduino(port, baudrate, serial::Timeout::simpleTimeout(1000));
+   
     cout << "Is the serial port open?";
     if (arduino.isOpen())
     {
@@ -70,12 +71,13 @@ int cast_cmd(int cmd)
 }
 
 
-void send_arduino_motor_cmd(serial::Serial arduino, int cmdl, int cmdr)
+void send_arduino_motor_cmd(int cmdl, int cmdr)
 {
     cmdl = cast_cmd(cmdl);
     cmdr = cast_cmd(cmdr);
 
     char cmd_str [50];
     sprintf(cmd_str, "M %3.3d %3.3d;", cmdl, cmdr);
+    arduino.write(cmd_str);
     cout << cmd_str << endl;
 }
