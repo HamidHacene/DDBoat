6ddd4adb221f7859054c3b376112d57c6671a9c7
ColinBaumgard <colin.baumgard@gmail.com>
2020-02-05 15:35:14 +0100

    ok

--- src/drivers/arduino_drivers/src/pid.cpp
+++ src/drivers/arduino_drivers/src/pid.cpp
@@ -1,15 +1,23 @@
-#include "ros/ros.h"
-#include <iostream>
-#include <stdio.h>
 #include <string>
-#include <arduino_drivers/Motor_dual.h>
+#include <iostream>
+#include <cstdio>
+#include <unistd.h>
+
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
+
+#include "arduino_drivers/Motor_dual.h"
+
+#define RATE 25
 
 double value_encoder_left, value_encoder_right;
 double value_cmd_left, value_cmd_right;
 double error_l, error_r;
 double previous_error_left = 0, previous_error_right = 0;
 double sum_error_left = 0, sum_error_right = 0;
-int rate = 25;
+
 
 double Kp, Ki, Kd;
 
@@ -33,8 +41,8 @@ void PID(double enc_left, double enc_right, double cmd_left, double cmd_right)
     sum_error_left = sum_error_left + error_left;
     sum_error_right = sum_error_right + error_right;
 
-    value_cmd_left = value_cmd_left + Kp * error_left + Kd * (error_left - previous_error_left) / rate + Ki * sum_error_left;
-    value_cmd_right = value_cmd_right + Kp * error_right + Kd * (error_right - previous_error_right) / rate + Ki * sum_error_right;
+    value_cmd_left = value_cmd_left + Kp * error_left + Kd * (error_left - previous_error_left) / RATE + Ki * sum_error_left;
+    value_cmd_right = value_cmd_right + Kp * error_right + Kd * (error_right - previous_error_right) / RATE + Ki * sum_error_right;
 
     previous_error_left = error_left;
     previous_error_right = error_right;
@@ -49,22 +57,28 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "pid_encoders");
     ros::NodeHandle n;
-    ros::NodeHandle node_priv("~");
-    ros::Rate loop_rate(rate);
+    
 
     ros::Publisher command_corrected = n.advertise<arduino_drivers::Motor_dual>("controlled_cmd_motor", 1000);
-    ros::Subscriber values_command = n.subscribe("motor_cmd", 1000, commandCallback);
-    ros::Subscriber values_encoders = n.subscribe("encoders", 1000, encodersCallback);
+    //ros::Subscriber values_command = n.subscribe("motor_cmd", 1000, commandCallback);
+    //ros::Subscriber values_encoders = n.subscribe("encoders", 1000, encodersCallback);
+
+    ros::Rate loop_rate(RATE);
+
+    arduino_drivers::Motor_dual cmd_corrected;
 
     while (ros::ok())
     {
         ros::spinOnce();
 
-        PID(value_encoder_left, value_encoder_right, value_cmd_left, value_cmd_right);
+        //PID(value_encoder_left, value_encoder_right, value_cmd_left, value_cmd_right);
 
-        arduino_drivers::Motor_dual cmd_corrected;
-        cmd_corrected.left = value_cmd_left;   //commande moteur gauche après pid
-        cmd_corrected.right = value_cmd_right; //commande moteur droite après pid
+        //cmd_corrected.left = value_cmd_left;   //commande moteur gauche après pid
+        //cmd_corrected.right = value_cmd_right; //commande moteur droite après pid
+
+        cmd_corrected.left = 100;   //commande moteur gauche après pid
+        cmd_corrected.right = 100;
+
         command_corrected.publish(cmd_corrected);
 
         loop_rate.sleep();
