4a58dd2279df73c61562445d47861c466ac20c64
ColinBaumgard <colin.baumgard@gmail.com>
2020-02-05 17:57:01 +0100

    setup add

--- external_libs/sudo_setup.bash
+++ external_libs/sudo_setup.bash
@@ -1,2 +1,8 @@
+source /opt/ros/kinetic/setup.bash
+unset https_proxy
+unset http_proxy
+
 cp -R /tmp/usr/local/include/* /usr/local/include/
-cp -R /tmp/usr/local/lib/* /usr/local/lib/ 
+cp -R /tmp/usr/local/lib/* /usr/local/lib/
+apt install libgps-dev
+ldconfig 
