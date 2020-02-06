b4fb8a9c89abddbf70b9d3c83610d987ef6713c3
ColinBaumgard <colin.baumgard@gmail.com>
2020-02-06 23:14:07 +0100

    automatic push

--- src/control/src/control.cpp
+++ src/control/src/control.cpp
@@ -1,5 +1,10 @@
 #include <cmath>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <string>
 #include "ros/ros.h"
 #include "std_msgs/Float64MultiArray.h"
 
@@ -11,68 +16,87 @@
 
 #include "eigen3/Eigen/Dense"
 
-# define M_PI           3.14159265358979323846  
+using namespace std;
 
+#define M_PI 3.14159265358979323846
 
 Eigen::Vector4d Xbateau = {0.0, 0.0, 0.0, 0.0};
-Eigen::Vector2d Xcible = {5, 5}; 
+Eigen::Vector2d Xcible = {5, 5};
 Eigen::Vector2d Vcible = {0, 0};
-Eigen::Vector2d Acible = {0, 0}; 
+Eigen::Vector2d Acible = {0, 0};
 
-Eigen::Vector2d u = {1,1};
+Eigen::Vector2d u = {1, 1};
 double e;
 double dcible;
-double x; 
-double y; 
-double theta ;
+double x;
+double y;
+double theta;
 double thetav;
-double v; 
+double v;
 
+ifstream waypoint_file("/home/colin/Documents/ENSTA/DDBoat/DDBoat/mission.txt");
+
+void waypoint()
+{
+    string line;
+    if (!getline(waypoint_file, line))
+    {
+        waypoint_file.clear();
+        waypoint_file.seekg(0, ios::beg);
+        getline(waypoint_file, line);
+    }
+    Xcible[0] = stod(line.substr(1, 5));
+    Xcible[1] = stod(line.substr(6, 10));
+}
 
 double sawtooth(double x)
 {
-  return 2.*atan(tan(x/2.));
-  //return fmod(x + M_PI, 2.*M_PI) - M_PI; // this is equivalent
+    return 2. * atan(tan(x / 2.));
+    //return fmod(x + M_PI, 2.*M_PI) - M_PI; // this is equivalent
 }
 
-void get_X_bateau(const std_msgs::Float64MultiArray msg){
-    Xbateau[0] =    msg.data[0]; //x
-    Xbateau[1] =     msg.data[1]; //y
-    Xbateau[2] =    msg.data[2]; //theta =  - thetaboussole -90
-    Xbateau[3] =    msg.data[3];  //v
-
-    }
-
-void get_position_cible(const geometry_msgs::Vector3 msg2){
-    Xcible[0] =  msg2.x;
-    Xcible[1] =  msg2.y;
+void get_X_bateau(const std_msgs::Float64MultiArray msg)
+{
+    Xbateau[0] = msg.data[0]; //x
+    Xbateau[1] = msg.data[1]; //y
+    Xbateau[2] = msg.data[2]; //theta =  - thetaboussole -90
+    Xbateau[3] = msg.data[3]; //v
 }
 
-void get_vitesse_cible(const geometry_msgs::Vector3 msg3){
-    Vcible[0] =  msg3.x;
-    Vcible[1] =  msg3.y;
+void get_position_cible(const geometry_msgs::Vector3 msg2)
+{
+    Xcible[0] = msg2.x;
+    Xcible[1] = msg2.y;
 }
 
-void get_acceleration_cible(const geometry_msgs::Vector3 msg4){
-    Acible[0] =  msg4.x;
-    Acible[1] =  msg4.y;
+void get_vitesse_cible(const geometry_msgs::Vector3 msg3)
+{
+    Vcible[0] = msg3.x;
+    Vcible[1] = msg3.y;
 }
 
-void controller(){
-    x = Xbateau[0]; 
+void get_acceleration_cible(const geometry_msgs::Vector3 msg4)
+{
+    Acible[0] = msg4.x;
+    Acible[1] = msg4.y;
+}
+
+void controller()
+{
+    x = Xbateau[0];
     y = Xbateau[1];
     theta = Xbateau[2];
     v = Xbateau[3];
     thetav = atan2(Xcible[0] - x, Xcible[1] - y);
     e = sawtooth(thetav - theta);
-    dcible = sqrt(pow(Xcible[1] - y,2)  + pow(Xcible[0] - x,2));
+    dcible = sqrt(pow(Xcible[1] - y, 2) + pow(Xcible[0] - x, 2));
 
     /*
     Eigen::Matrix2d A;
     A << ct - v*st, ct + v*st,
          st + v*ct, st - v*ct; 
 
-    Eigen::Vector2d B = {-std::abs(v)*v*ct, -std::abs(v)*v*st};
+    Eigen::Vector2d B = {-abs(v)*v*ct, -abs(v)*v*st};
     Eigen::Vector2d Y = {x, y};
     Eigen::Vector2d dY = {v*ct, v*st};
     Eigen::Vector2d a = {1, 2};
@@ -83,11 +107,9 @@ void controller(){
     u = A.fullPivLu().solve(z - B);
     */
 }
-    
 
-
-   
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
     // INITIALISATION
     ros::init(argc, argv, "control");
@@ -103,7 +125,7 @@ int main(int argc, char **argv){
     ros::Subscriber sub4 = vcible.subscribe("acceleration_cible", 1000, get_acceleration_cible);
 
     //DECLARATION DE PUBLISHERS
-    
+
     ros::NodeHandle commande;
     ros::NodeHandle xbateau_visu;
     ros::NodeHandle node_handle;
@@ -118,16 +140,15 @@ int main(int argc, char **argv){
     marker.type = visualization_msgs::Marker::MESH_RESOURCE;
     marker.action = visualization_msgs::Marker::ADD;
 
-    
     ros::Rate loop_rate(10);
-    while (ros::ok()){
-        
+    while (ros::ok())
+    {
         ros::spinOnce();
         controller();
         geometry_msgs::PoseStamped pose;
         std_msgs::Float64MultiArray com;
 
-        std::vector<double> X_control = {e,thetav};
+        vector<double> X_control = {e, thetav};
         com.data.clear();
         com.data.insert(com.data.end(), X_control.begin(), X_control.end());
 
@@ -139,7 +160,7 @@ int main(int argc, char **argv){
         pose.pose.position.y = Xbateau[1];
         pose.pose.position.z = 0;
         tf::Quaternion q;
-        q.setRPY(0,0,Xbateau[2]);
+        q.setRPY(0, 0, Xbateau[2]);
         tf::quaternionTFToMsg(q, pose.pose.orientation);
         //3D
 
@@ -147,15 +168,14 @@ int main(int argc, char **argv){
 
         marker.pose = pose.pose;
         double scale = 0.4;
-        marker.scale.x = scale*1;
-        marker.scale.y = scale*1;
-        marker.scale.z = scale*1;
+        marker.scale.x = scale * 1;
+        marker.scale.y = scale * 1;
+        marker.scale.z = scale * 1;
         marker.color.a = 1.0; // alpha = transparence
         marker.color.r = 1.0;
         marker.color.g = 1.0;
         marker.color.b = 1.0;
         marker.mesh_resource = "package://control/meshs/boat.dae";
-
 
         vis_pub.publish(marker);
 
@@ -166,10 +186,15 @@ int main(int argc, char **argv){
         ROS_INFO("position x : %f", Xbateau[0]);
         ROS_INFO("position  y: %f", Xbateau[1]);
         ROS_INFO(" angle theta : %f", Xbateau[2]);
-        ROS_INFO(" vitesse : %f",Xbateau[3]);
-        
+        ROS_INFO(" vitesse : %f", Xbateau[3]);
+
+        if(dcible < 4)
+        {
+            waypoint();
+        }
+
         loop_rate.sleep();
-
     }
 
-    }
+    waypoint_file.close();
+}
