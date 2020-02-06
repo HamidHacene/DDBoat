37e336cc207bb9ce360f93f4c749de7a4900752d
ColinBaumgard <colin.baumgard@gmail.com>
2020-02-06 19:12:50 +0100

    automatic push

--- src/filters/src/kalman.cpp
+++ src/filters/src/kalman.cpp
@@ -55,6 +55,10 @@ void capCallback(const std_msgs::Float64::ConstPtr &msg)
 {
     double thetaB = msg->data;
     heading = (90 - thetaB)*M_PI/180.;
+    if(heading < -M_PI)
+    {
+        heading = 2*M_PI + heading;
+    }
 }
 
 int main(int argc, char **argv)
