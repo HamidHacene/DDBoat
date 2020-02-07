fbf8634fb8bf075d74bcb68536fa9368130966a9
LudovicDiguet <lludovic.diguet@ensta-bretagne.org>
2020-02-07 09:41:35 +0100

    input-prportionel

--- src/control/src/motor_input.cpp
+++ src/control/src/motor_input.cpp
@@ -16,13 +16,8 @@ double vitesse_gauche;
 
 
 void conversion(){
-    double K = 1;
-    if (u[1] > 4){
-    v[0] = (u[0]-2,5)*(46) + 127;
-    v[1] = - (u[0]-2,5)*(46) + 127;}
-    else {
-    v[0] = 0;
-    v[1] = 0;
+    v[0] = 160 - (u[0]*80/M_1_PI) ;
+    v[1] = 160 + (u[0]*80/M_1_PI) ;
     }
     
 }
